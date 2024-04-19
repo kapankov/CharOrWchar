7ab1f5c0c0b17ddcfcdd0a898f369a5a9082ea42
Konstantin A. Pankov <konstantin.pankov@fsight.ru>
2024-04-19 17:13:24 +0500

    _CT macro

--- CharOrWchar.cpp
+++ CharOrWchar.cpp
@@ -1,15 +1,32 @@
+/*
+	This example demonstrates the use of the _CT macro in templates where you need to define string literal types.
+	Konstantin A. Pankov, 2024, explorus@mail.ru
+*/
+
 #include <iostream>
 
-//#define _TT( T, V ) NarrowOrWide<T>((V), _T(V))
-#define _TT( V ) NarrowOrWide<T>((V), _T(V))
+#ifndef _T
+#define _T(x) L##x
+#endif
+
+namespace detail
+{
+	template<typename T, typename C, typename W>
+	decltype(auto) NarrowOrWide(C&& c, W&& w)
+	{
+		if constexpr (std::is_same_v<typename std::remove_reference_t<typename std::remove_const_t<T>>, wchar_t>)
+			return w;
+		else
+			return c;
+	}
+} // end of detail namespace
+
+#define _CT( T, V ) (NarrowOrWide<T>(V, _T(V)))
 
 template<typename T>
 void appendFoo(std::basic_string<T>& str)
 {
-	if constexpr (std::is_same_v<T, wchar_t>)
-		str += L"Foo";
-	else
-		str += "Foo";
+	str += _CT(T, "Foo");
 }
 
 int main()
