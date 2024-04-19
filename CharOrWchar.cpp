8513f9003895adb320f4fa23856a5fd83a8f50a3
Konstantin A. Pankov <konstantin.pankov@fsight.ru>
2024-04-19 14:03:42 +0500

    constexpr

--- CharOrWchar.cpp
+++ CharOrWchar.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 //#define _TT( T, V ) NarrowOrWide<T>((V), _T(V))
+#define _TT( V ) NarrowOrWide<T>((V), _T(V))
 
 template<typename T>
 void appendFoo(std::basic_string<T>& str)
 {
-	str += "Foo";
+	if constexpr (std::is_same_v<T, wchar_t>)
+		str += L"Foo";
+	else
+		str += "Foo";
 }
 
 int main()
@@ -13,4 +17,9 @@ int main()
 	std::string str{ "append" };
 	appendFoo(str);
     std::cout << str << "\n";
+
+	std::wstring wstr{ L"append" };
+	appendFoo(wstr);
+	std::wcout << wstr << "\n";
+
 }
